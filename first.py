11a1dd7ee9c3a6fb293feb5fbfd72e58cb14bd1c
stone <stoneaz@mail.ru>
2020-11-25 15:12:48 +0300

    coffee

--- first.py
+++ first.py
@@ -1,42 +1,39 @@
-import sys, math
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton
-from PyQt5.QtCore import Qt, QPointF
-from PyQt5.QtGui import QPainter, QColor, QPolygon, QPen
-from random import randint
+import sqlite3
+import sys
+
 from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
-class Example(QWidget):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.flag = False
-        self.pushButton = QPushButton('кнопка', self)
-        self.pushButton.setGeometry(280, 510, 93, 28)
-        self.pushButton.clicked.connect(self.run)
+        uic.loadUi("1234.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.update_result)
+        self.modified = {}
+        self.titles = None
 
-    def run(self):
-        self.flag = True
-        self.update()
-
-    def draw(self, qp):
-        R, G, B = randint(0, 255), randint(0, 255), randint(0, 255)
-        pn = QPen(QColor(R, G, B))
-        self.qp.setPen(pn)
-        x = randint(50, 300)
-        y = randint(50, 300)
-        k = randint(50, 300)
-        self.qp.drawEllipse(x, y, k, k)
-
-    def paintEvent(self, event):
-        if self.flag:
-            self.qp = QPainter()
-            self.qp.begin(self)
-            self.draw(self.qp)
-            self.qp.end()
+    def update_result(self):
+        cur = self.con.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
+        result = cur.execute("SELECT * FROM types_coffee WHERE ID > 0").fetchall()
+        # Заполнили размеры таблицы
+        self.tableWidget.setRowCount(len(result))
+        # Если запись не нашлась, то не будем ничего делать
+        if not result:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        self.tableWidget.setColumnCount(len(result[0]))
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = MyWidget()
     ex.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
